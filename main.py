732ad25611dffec8db6650f8db9825a4c235c193
Angelus123 <izerefaifelix@gmail.com>
2021-10-15 18:50:29 +0200

    Get list of port of arduino

--- main.py
+++ main.py
@@ -1,12 +1,27 @@
-import serial
-import time
-arduino = serial.Serial(port='COM3', baudrate=115200, timeout=.1)
-def write_read(x):
-    arduino.write(bytes(x, 'utf-8'))
-    time.sleep(0.05)
-    data = arduino.readline()
-    return data
-while True:
-    num = input("Enter a number: ") # Taking input from user
-    value = write_read(num)
-    print(value) # printing the va
+# import serial
+# import time
+
+# arduino = serial.Serial(port='COM3', baudrate=9600, timeout=.1)
+# time.sleep(2)
+
+# print("Presione 1 para Encender y 0 para Apagar el LED:  ")
+# print (f'{arduino.readline()}')                            #read the serial data and print it as line 
+# print ("You have new message from Arduino")
+
+# while 1:
+
+#     datousuario = input()
+
+#     if datousuario == '1':
+#         arduino.write(b'1')
+#         print("LED Encendido")
+#     elif datousuario == '0':
+#         arduino.write(b'0')
+#         print("LED Apagado")
+import serial.tools.list_ports
+ports = serial.tools.list_ports.comports()
+serialInst = serial.Serial()
+portList = []
+for onePort in ports:
+    portList.append(str(onePort))
+    print(str(onePort))
